b1f21236ad42f50e020dda29cd2d5e57a4a43a7f
Hung <mqhung93@gmail.com>
2020-11-09 16:40:20 +0700

    thuc hanh khai bao bien va toan tu

---
+++ toantu.js
@@ -0,0 +1,8 @@
+let inputwidth;
+let inputlength;
+inputwidth = prompt("Input the width");
+inputlength= prompt("Input the length");
+let width = parseInt(inputwidth);
+let length = parseInt(inputlength);
+let area = width * length;
+document.write("The area is: "+area);
